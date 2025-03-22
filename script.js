49f523c413aa63d5bac720592dfb8461390fbeff
Deniss Kuzmins <deniss.kuzmins@elev.ga.ntig.se>
2025-03-22 12:39:20 +0100

    Försätt jobba med varukorget funktion. och påbörjade cart.html

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-
+// Funktion för att uppdatera kundvagnsantalet
+function updateCartCount() {
+    const cartCount = document.querySelector('.cart-count');
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cartCount.textContent = cart.length;
+}
 // Funktion för att lägga till produkter i varukorgen
 function addToCart(productName, price, imageUrl) {
     let cart = JSON.parse(localStorage.getItem('cart')) || [];
